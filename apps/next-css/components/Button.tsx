36f1a6c5cfe5576069f8e05a066a16ff3ca2da10
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-01-28 12:02:38 +0900

    feat: css module example

---
+++ apps/next-css/components/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Button.module.css'
+
+export const Button = () => {
+  return (
+    <button type={'button'} className={styles.error}>Destroy</button>
+  )
+}
